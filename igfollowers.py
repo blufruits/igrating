bf06953de7f23393744a3d4ffd5a14c8fec66e1d
blufruits <64915122+blufruits@users.noreply.github.com>
2020-09-01 00:00:19 +0100

    Updating notation

--- igfollowers.py
+++ igfollowers.py
@@ -1,18 +1,29 @@
 import matplotlib.pyplot as plt
+import math
 
-initial_followers = int(input("Current followers ")) #attributing the number of followers to the variable 'initial_followers'
-initial_following = int(input("Current following ")) #attributing the number of accounts followed to the variable 'inital_following'
-    
+initial_followersnotrunc = float(input("Current followers ")) #attributing the number of followers to the variable 'initial_followers'
+initial_followingnotrunc = float(input("Current following ")) #attributing the number of accounts followed to the variable 'inital_following'
+
+initial_followers = math.trunc(initial_followersnotrunc) #removing fractional part if need be. 
+initial_following = math.trunc(initial_followingnotrunc)
 
 if initial_followers ==0 and initial_following == 0: #deals with /0 error. If only one variable is 0 no /0 error can occur (look below)
+    print ("No rating can be generated")
+    exit()
+elif initial_followers < 0 or initial_following < 0:
     print ("No rating can be generated")
     exit()
 else:
     pass
     
-total_people = int(input
-                   ("For how many people do you want to simulate for? ")) #attributing the number of 'follow them and they follow back' scenarios to the 'total_people' variable
-
+total_peoplenotrunc = int(input
+                   ("How many 'mutal' people should the graph simulate? ")) #attributing the number of 'follow them and they follow back' scenarios to the 'total_people' variable
+total_people = math.trunc(total_peoplenotrunc)
+if total_people < 0:
+    print ("No rating can be generated")
+    exit()
+else:
+    pass
 #############################################################################################################################################    
 
 n = [] #defining the x axis list
@@ -38,15 +49,15 @@ else:
 ############################################################################################################################################# 
 
 plt.plot(n,ratios) #plotting graph
-plt.xlabel("Additional followers who you follow back")
+plt.xlabel("Number of 'mutal' accounts followed")
 plt.ylabel("Instagram rating (higher is better)")
 plt.yticks([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
 if reciprocal == True:
     plt.title("""(A + n)/(B + n)    
-A = """+str(initial_following)+"  |  B = "+str(initial_followers)+ "  |  0≤n≤" +str(total_people))
+A = """+str(initial_following)+"  |  B = "+str(initial_followers)+ "  |  0≤n≤" +str(total_people)+ "  |  A,B,n ∈ Z*")
 elif reciprocal == False:
     plt.title("""(A + n)/(B + n)    
-A = """+str(initial_followers)+"  |  B = "+str(initial_following)+ "  |  0≤n≤" +str(total_people))
+A = """+str(initial_followers)+"  |  B = "+str(initial_following)+ "  |  0≤n≤" +str(total_people)+ "  |  A,B,n ∈ Z*")
 else:
     pass
 
